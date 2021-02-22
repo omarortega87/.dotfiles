ceb3d84e529c268a22bfb83a6301e8cc167f33b7
Omar Ortega <omarortega@pop-os.localdomain>
2021-02-22 14:49:50 -0600

    Updating install.sh

--- install.sh
+++ install.sh
@@ -66,8 +66,8 @@ sudo apt install maven
 
 sudo apt install neovim
 
-curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     
 mkdir ~/.config/nvim
 
